353c520816dc7aa16a52034322ba31e74b069241
John Francis Lomeda <kokoylemonada@gmail.com>
2024-04-30 13:07:24 +0800

    create

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Activity 6 Lomeda</title>
+</head>
+
+<body>
+    <h1>My Activity 6</h1>
+    <ul>
+        <li><a href="/posts/1">Post 1</a></li>
+        <li><a href="/posts/2">Post 2</a></li>
+    </ul>
+
+</body>
+
+</html>
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>My Posts</title>
+    </head>
+    <body>
+        <h1> This is my post {{$post}}</h1>
+    </body>
+</html>
